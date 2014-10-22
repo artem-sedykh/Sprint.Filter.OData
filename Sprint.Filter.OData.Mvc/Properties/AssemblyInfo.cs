bfd70db60d7b3a49ee39144799d18f1d6e912ea5
artem-sedykh <anavrin72@gmail.com>
2014-10-22 13:19:46 +0400

    update

--- Sprint.Filter.OData.Mvc/Properties/AssemblyInfo.cs
+++ Sprint.Filter.OData.Mvc/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sprint.Filter.OData.Mvc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sprint.Filter.OData.Mvc")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sprint.Filter.OData.Mvc")]
